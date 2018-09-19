644279fc19eeee446c318971e128241a3fcc6e0b
juanssa <juanssa91@gmail.com>
2018-09-18 21:13:07 -0500

    formulario agregar cliente

--- php/iniciar_sesion.php
+++ php/iniciar_sesion.php
@@ -18,7 +18,7 @@ $filas = mysqli_num_rows($resultado);
 
 if($filas > 0){
     echo $filas;
-    header("Location: inicio.html");
+    header("Location: ../inicio.html");
 }else{
     echo "Error en la autenticacion... intente de nuevo <a href='index.html'> regresar</a>";
     
